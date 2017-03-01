c5bf5db4ca2c4c5681b803f58c05d6a9a3e19ee1
ingbrzy <ingbrzy@miuios.cz>
2017-03-01 21:39:02 +0100

    add en GB US SK

---
+++ Slovak/RRO-2.0-SK/CalendarProvider.apk/RRO.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+up=/home/ingbrzy/ZenWatch3-SLOVAK/Slovak
+
+aapt=$up/tools/aapt
+
+testkey_pem=$up/tools/security/testkey.x509.pem;
+testkey_pk8=$up/tools/security/testkey.pk8;
+
+key_pem=$testkey_pem;
+key_pk8=$testkey_pk8;
+
+
+echo "Creating R.java from strings";
+
+$aapt package -m -J gen/ -M AndroidManifest.xml -S res/ -I $up/tools/android.jar
+
+echo "Building overlay.apk";
+
+$aapt package -f -M AndroidManifest.xml -S res/ -I $up/tools/android.jar -F CalendarProvider-overlay.apk
+
+echo "Signing overlay.apk";
+
+java -Xmx4048m -jar $up/tools/apksigner.jar sign --key $key_pk8 --cert $key_pem CalendarProvider-overlay.apk			
+
+echo "RRO apk is done!";
